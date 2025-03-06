6761787467c049f8415b544211ca1488797aa862
Raghul <raghulchandramouli@gmail.com>
2025-03-06 13:33:55 +0530

    server initialized with a page

---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+import socket
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8080
+
+server_socket = socket.socket(socket.AF_INET, 
+                              socket.SOCK_STREAM)
+
+server_socket.setsockopt(socket.SOL_SOCKET,
+                         socket.SO_REUSEADDR, 
+                         1) # reuse address
+
+server_socket.bind((SERVER_HOST, 
+                    SERVER_PORT))
+
+server_socket.listen(5) # backlog parameter establishes the queue
+
+print(f"Listening on port {SERVER_PORT} ...")
+
+while True:
+    
+    client_socket, client_address = server_socket.accept()
+    request = client_socket.recv(1500).decode()
+    
+    print(request)
+    headers = request.split('\n')
+    
+    first_header_components = headers[0].split()
+
+    http_method = first_header_components[0]
+    path = first_header_components[1]
+    
+    if path == '/':
+        fin = open('index.html')
+        content = fin.read()
+        fin.close()
+        
+        
